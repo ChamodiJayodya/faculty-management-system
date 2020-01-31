8f3b7c551e58756dae97f356e04eaa32f06060be
Chamodi <jayodyachamodi@gmail.com>
2020-01-31 09:50:11 +0530

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.sql.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Dasuni
+ */
+public class Main {
+    public static void main(String args[]){
+        Start x = new Start();
+        x.setVisible(true);
+    
+    }
+    
+}
